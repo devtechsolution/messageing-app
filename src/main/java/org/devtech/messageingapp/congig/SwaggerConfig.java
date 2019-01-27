8e70ae9619ae3727572a4ec4ff74b17fb8cb68e9
devtechsolution <srivastvadevzone@gmail.com>
2019-01-27 21:26:06 +0530

    Update SwaggerCofing: producers and consumer data format

--- src/main/java/org/devtech/messageingapp/congig/SwaggerConfig.java
+++ src/main/java/org/devtech/messageingapp/congig/SwaggerConfig.java
@@ -8,6 +8,10 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 //Configuration
 @Configuration
 //Enable Swagger
@@ -20,6 +24,8 @@ public class SwaggerConfig {
             "Social Media API", "Social Media API",
             "1.0", "urn:tos", DEFAULT_CONTACT, "Apache 2.0",
             "http://www.apache.org/licenses/LICENSE-2.0");
+    private static final Set<String> DEFAULT_PRODUCES_AND_CONSUMES = new HashSet<>(
+            Arrays.asList("application/json", "application/xml"));
 
 
     //Define Bean- Docket
@@ -30,6 +36,8 @@ public class SwaggerConfig {
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
-                .apiInfo(DEFAULT_API_INFO);
+                .apiInfo(DEFAULT_API_INFO)
+                .produces(DEFAULT_PRODUCES_AND_CONSUMES)
+                .consumes(DEFAULT_PRODUCES_AND_CONSUMES);
     }
 }
