b99d6258b0dab36aeb30f82d6d60168cc6c6e213
devtechsolution <srivastvadevzone@gmail.com>
2019-01-27 20:51:30 +0530

    Update SwaggerCofing Java file to show contact parameters

--- src/main/java/org/devtech/messageingapp/congig/SwaggerConfig.java
+++ src/main/java/org/devtech/messageingapp/congig/SwaggerConfig.java
@@ -2,6 +2,8 @@ package org.devtech.messageingapp.congig;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -12,13 +14,22 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 public class SwaggerConfig {
 
+    public static final Contact DEFAULT_CONTACT =new Contact(
+            "Aditya Srivastva", "https://github.com/devtechsolution", "aditya.srivastva103029@gmail.com");
+    public static final ApiInfo DEFAULT_API_INFO =new ApiInfo(
+            "Social Media API", "Social Media API",
+            "1.0", "urn:tos", DEFAULT_CONTACT, "Apache 2.0",
+            "http://www.apache.org/licenses/LICENSE-2.0");
+
+
     //Define Bean- Docket
-        //Configure
-            //Swager2
-            //All the Paths
-            //All the apis
+    //Configure
+    //Swager2
+    //All the Paths
+    //All the apis
     @Bean
-    public Docket api(){
-        return  new Docket(DocumentationType.SWAGGER_2);
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(DEFAULT_API_INFO);
     }
 }
